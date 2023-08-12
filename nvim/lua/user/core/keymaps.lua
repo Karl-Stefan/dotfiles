83c5369ef445fe27d9dbe665ad8731598f406108
Karl Stefan Afradi <afradi@pm.me>
2023-08-12 23:58:26 +0200

    Modified keybindings to be more consistent with other software

--- nvim/lua/user/core/keymaps.lua
+++ nvim/lua/user/core/keymaps.lua
@@ -10,5 +10,5 @@ vim.keymap.set('n', '<leader>l', ':nohl<CR>') -- Clear search
 
 -- Plugin keymaps
 vim.keymap.set('n', '<C-n>', ':NvimTreeFocus<CR>', { silent = true } ) -- Toggle NvimTree
-vim.keymap.set('n', '<C-l>', ':BufferLineCycleNext<CR>', { silent = true } ) -- Toggle NvimTree
-vim.keymap.set('n', '<C-h>', ':BufferLineCyclePrev<CR>', { silent = true } ) -- Toggle NvimTree
+vim.keymap.set('n', '<C-k>', ':BufferLineCycleNext<CR>', { silent = true } ) -- Toggle NvimTree
+vim.keymap.set('n', '<C-j>', ':BufferLineCyclePrev<CR>', { silent = true } ) -- Toggle NvimTree
